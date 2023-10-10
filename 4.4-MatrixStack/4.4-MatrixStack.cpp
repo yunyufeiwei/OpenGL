4acb8001b9b6ac75812456a69e5fa7b957f7c462
IceRain <328800436@qq.com>
2023-10-10 20:10:13 +0800

    新增堆栈代码

--- 4.4-MatrixStack/4.4-MatrixStack.cpp
+++ 4.4-MatrixStack/4.4-MatrixStack.cpp
@@ -9,15 +9,13 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include "Utils.h"
+#include <stack>
 
 #define numVAOs 1
 #define numVBOs 2
 
 Utils util = Utils();
 float cameraX, cameraY, cameraZ;
-float cubeLocX, cubeLocY, cubeLocZ;
-float pyrLocX, pyrLocY, pyrLocZ;
-
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
@@ -27,6 +25,8 @@ GLuint mvLoc, projLoc;
 int width, height;
 float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat;  //定义 v矩阵（视图矩阵),p矩阵（投影矩阵）,mv矩阵（模型-视图矩阵）
+
+stack<glm::mat4>mvStack;
 
 //声明一个名为setupVertices的数组，包含36个组成立方体的顶点
 void setupVertices(void)
@@ -71,68 +71,89 @@ void setupVertices(void)
 void init(GLFWwindow* window)
 {
 	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
-	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
-	cubeLocX = 0.0f, cubeLocY = -2.0f, cubeLocZ = 0.0f;		//设置cube生成的位置
-	pyrLocX = 2.0f, pyrLocY = 2.0f, pyrLocZ = 0.0f;			//设置金字塔生成的位置
+
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 12.0f;
+
 	setupVertices();
 }
 
 void display(GLFWwindow* window, double currentTime)
 {
-	////每帧之前清除深度缓冲区
+	//每帧之前清除深度缓冲区
 	glClear(GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	glUseProgram(renderingProgram);
 
 	//获取MV矩阵和投影矩阵的统一变量
 	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-
-	//构建透视矩阵
-	glfwGetFramebufferSize(window, &width, &height);
-	aspect = (float)width / (float)height;
-	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);   //1.0472 radians = 60 degrees  glm::perspective(<filed of view> , <aspect ratio> , <near plane>)
-
+	
 	//构建视图矩阵、模型矩阵和视图-模型矩阵
 	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mvStack.push(vMat);		//将视图矩阵推入堆栈
 
-	//--------绘制正方体
-	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
-	mvMat = vMat * mMat;
-
-	//将透视矩阵和MV矩阵复制给相应的统一变量
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-	//将VBO关联给顶点着色器中相应的顶点属性
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	//调整OpenGL设置，绘制模型
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-
-	glDrawArrays(GL_TRIANGLES, 0, 36);				//Display()函数做得最后一件事就是通过调用glDrawArrays()来绘制模型，指定模型的组成方式(三角形),并设置顶点数。
-
-	//-------------绘制金字塔
-	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(pyrLocX, pyrLocY, pyrLocZ));
-	mvMat = vMat * mMat;
+	//--------金字塔模型==太阳
+	mvStack.push(mvStack.top());
+	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));		//太阳位置
+	mvStack.push(mvStack.top());
+	mvStack.top() *= glm::rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(1.0f, 1.0f, 1.0f));
 
 	//将透视矩阵和MV矩阵复制给相应的统一变量
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
 	//将VBO关联给顶点着色器中相应的顶点属性
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
-
 	//调整OpenGL设置，绘制模型
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 18);				//Display()函数做得最后一件事就是通过调用glDrawArrays()来绘制模型，指定模型的组成方式(三角形),并设置顶点数。
+	mvStack.pop();									//从堆栈中移除太阳的轴旋转
 
-	glDrawArrays(GL_TRIANGLES, 0, 18);
+	//-----------------------  cube == planet  
+	mvStack.push(mvStack.top());
+	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(sin((float)currentTime) * 4.0, 0.0f, cos((float)currentTime) * 4.0));
+	mvStack.push(mvStack.top());
+	mvStack.top() *= rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0, 1.0, 0.0));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+	glEnableVertexAttribArray(0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	mvStack.pop();
 
+	//-----------------------  smaller cube == moon
+	mvStack.push(mvStack.top());
+	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, sin((float)currentTime) * 2.0, cos((float)currentTime) * 2.0));
+	mvStack.top() *= rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0, 0.0, 1.0));
+	mvStack.top() *= scale(glm::mat4(1.0f), glm::vec3(0.25f, 0.25f, 0.25f));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+	glEnableVertexAttribArray(0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	mvStack.pop(); mvStack.pop(); mvStack.pop();
+	mvStack.pop();  // the final pop is for the view matrix
+
+}
+
+void window_size_callback(GLFWwindow* win, int newWidth, int newHeight)
+{
+	aspect = (float)newWidth / (float)newHeight;
+	glViewport(0, 0, newWidth, newHeight);
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);   //1.0472 radians = 60 degrees  glm::perspective(<filed of view> , <aspect ratio> , <near plane>)
 }
 
 int main(void)
@@ -150,6 +171,8 @@ int main(void)
 	}
 
 	glfwSwapInterval(1);
+
+	glfwSetWindowSizeCallback(window, window_size_callback);
 
 	//执行初始化函数，需要传window的参数进来
 	init(window);
